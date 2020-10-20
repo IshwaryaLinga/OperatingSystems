2218549f9d4e05acaa72cd37ee594f8ea8225694
IshwaryaLinga2001 <71553271+IshwaryaLinga2001@users.noreply.github.com>
2020-10-20 13:39:54 +0530

    Memory management via fork()

---
+++ fork(a).c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h> 
+#include <sys/types.h> 
+int main() 
+{ 
+    fork(); 
+    fork(); 
+    fork(); 
+    printf("hello\n"); 
+    return 0; 
+} 
