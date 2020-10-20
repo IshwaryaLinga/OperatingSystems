c471d8b747ee7832dd439ec3401f91887c2061f4
IshwaryaLinga2001 <71553271+IshwaryaLinga2001@users.noreply.github.com>
2020-10-20 13:46:19 +0530

    Memory management via exec()

---
+++ exec.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h> 
+#include<unistd.h> 
+
+int main() 
+{ 
+    int i; 
+
+    printf("I am EXEC.c called by execvp() "); 
+    printf("\n"); 
+
+    return 0; 
+} 
